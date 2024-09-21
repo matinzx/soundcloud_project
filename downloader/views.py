426c885ca40d2175918d94f72bdf95602335b35b
katin <mtynkably@gmail.com>
2024-09-22 02:07:32 +0330

    ok-non-cover

--- downloader/views.py
+++ downloader/views.py
@@ -2,16 +2,19 @@ from django.shortcuts import render
 from django.http import HttpResponse, FileResponse
 import youtube_dl
 import os
-import subprocess
 import eyed3
 
+# تابع برای اضافه کردن تصویر کاور به فایل MP3
 def add_image(art_image_path, song_filename, mime_type='image/jpeg'):
     audiofile = eyed3.load(song_filename)
     if audiofile.tag is None:
         audiofile.initTag()
-    audiofile.tag.images.set(3, open(art_image_path, 'rb').read(), mime_type)
+    # افزودن تصویر کاور به فایل صوتی
+    with open(art_image_path, 'rb') as image_file:
+        audiofile.tag.images.set(3, image_file.read(), mime_type)
     audiofile.tag.save()
 
+# ویوی اصلی
 def index(request):
     if request.method == 'POST':
         track_url = request.POST.get('track_url')
@@ -21,18 +24,19 @@ def index(request):
 
         try:
             ydl_opts = {
-                'quiet': False,
-                'proxy': 'http://192.168.202.121:10809',
+                'quiet': False,  # برای مشاهده لاگ دانلود
+                'proxy': 'http://192.168.202.121:10809',  # پروکسی اگر لازم باشد
                 'format': 'bestaudio/best',
                 'postprocessors': [{
                     'key': 'FFmpegExtractAudio',
                     'preferredcodec': 'mp3',
                     'preferredquality': '192',
                 }],
-                'outtmpl': '%(title)s.%(ext)s',
-                'writethumbnail': True,
+                'outtmpl': '%(title)s.%(ext)s',  # نام فایل خروجی
+                'writethumbnail': True,  # دانلود کاور تصویر
             }
 
+            # دانلود فایل صوتی و کاور تصویر
             with youtube_dl.YoutubeDL(ydl_opts) as ydl:
                 info_dict = ydl.extract_info(track_url, download=True)
                 filename = ydl.prepare_filename(info_dict)
@@ -40,9 +44,7 @@ def index(request):
                 response_filename = os.path.abspath(f"{base_filename}.mp3")
                 cover_path = os.path.abspath(f"{base_filename}.jpg")
 
-                artist = info_dict.get('artist', 'Unknown Artist')
-                album = info_dict.get('album', 'Unknown Album')
-
+                # بررسی وجود فایل صوتی و کاور
                 if not os.path.exists(response_filename):
                     return HttpResponse("Error: Audio file not found.", status=500)
                 if not os.path.exists(cover_path):
@@ -51,6 +53,7 @@ def index(request):
                 # افزودن کاور به فایل صوتی
                 add_image(cover_path, response_filename)
 
+                # بازگشت فایل MP3 به کاربر به عنوان پاسخ دانلودی
                 return FileResponse(open(response_filename, 'rb'), as_attachment=True, filename=os.path.basename(response_filename))
 
         except Exception as e:
